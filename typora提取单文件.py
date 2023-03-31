a22cc76026c1e418634396d0257f6fd39993815d
weiguowen <weiguowen@sointech.com.cn>
2023-03-31 16:26:21 +0800

    提取单文件，只针对图片存放在同一目录内的assets文件夹的情况

--- typora提取单文件.py
+++ typora提取单文件.py
@@ -1,20 +1,23 @@
 # 提取单个markdown文件
 import os
+import shutil
 
 
 def create_new_file(md_path):
     '''
     创建新文件夹
     '''
-    os.makedirs(md_path[:-3] + '/assets')
-    return md_path[:-3] + '/assets/'
+    img_path = md_path[:-3] + '/assets/'
+    file_path = md_path[:-3]
+    os.makedirs(img_path, mode=0o777)
+    return [file_path,img_path]
 
 
 def move_img(md_path, new_img_path):
     '''
     移动图片
     '''
-    with open(md_path, 'r') as file:
+    with open(md_path, 'r', encoding='utf-8') as file:
         md_lines = file.readlines()
     for md_line in md_lines:
         relative_img_path = ''
@@ -23,15 +26,38 @@ def move_img(md_path, new_img_path):
             relative_img_path = md_line.split('](')[1].split(')')[0]
             n = relative_img_path.rfind('/')
             file_name = relative_img_path[n + 1:]
-        front_path = md_path[:md_path.rfind('\\')]
+        else:
+            continue
+        front_path = md_path[:md_path.rfind('\\')+1]
         while relative_img_path.startswith('..'):
             front_path = find_last_path(front_path)
-            relative_img_path = relative_img_path[relative_img_path.find('./')+2:]
+            relative_img_path = relative_img_path[relative_img_path.find('./') + 2:]
         img_path = front_path + relative_img_path
-        # img_file = open(img_path, 'r')
-        # new_img_file = open(new_img_path + file_name, 'w')
-        os.system('xcopy ' + img_path + ' ' + new_img_path)
+        shutil.copy2(img_path, new_img_path)
 
+def move_md(md_path, file_path):
+    '''
+    移动markdown文件
+    '''
+    shutil.copy2(md_path, file_path)
+
+def modify_md(md_path):
+    '''
+    当图片存在非同文件夹下的assets时修改图片路径
+    '''
+    with open(md_path, 'r+', encoding='utf-8') as file:
+        md_lines = file.readlines()
+    for md_line in md_lines:
+        relative_img_path = ''
+        file_name = ''
+        if '![image-' in md_line and '](' in md_line and ')' in md_line:
+            relative_img_path = md_line.split('](')[1].split(')')[0]
+            n = relative_img_path.rfind('/')
+            file_name = relative_img_path[n + 1:]
+            new_line = f'![{file_name}](assets/{file_name})'
+            file.write(new_line)
+        else:
+            continue
 
 def find_last_path(path):
     '''
@@ -43,6 +69,7 @@ def find_last_path(path):
 
 if __name__ == '__main__':
     md_path = input('输入要提取的markdown文件目录:')
-    new_img_path = create_new_file(md_path)
-    move_img(md_path, new_img_path)
+    new_path = create_new_file(md_path)
+    move_img(md_path, new_path[1])
+    move_md(md_path, new_path[0])
     print('success')
